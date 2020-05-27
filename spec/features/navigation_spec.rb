4b09c93a4d3b1a789f2c6ae28b98a89b82175211
Ana Perez Santos <a.jperez@yahoo.com>
2020-05-27 17:10:18 -0600

    Update test with default user functionality

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -49,8 +49,61 @@ RSpec.describe 'Site Navigation' do
       within 'nav' do
         expect(page).to have_content("Cart: 0")
       end
+    end
+  end
 
+  describe 'As a User' do
+    it "I see a nav bar with links to all pages" do
+      visit '/merchants'
+
+      within 'nav' do
+        click_link 'All Items'
+      end
+
+      expect(current_path).to eq('/items')
+
+      within 'nav' do
+        click_link 'All Merchants'
+      end
+
+      expect(current_path).to eq('/merchants')
+
+      within 'nav' do
+        click_link 'Home'
+      end
+
+      expect(current_path).to eq('/')
+
+      within 'nav' do
+        click_link 'My Profile'
+      end
+
+      expect(current_path).to eq('/profile')
+
+      within 'nav' do
+        click_link 'Log Out'
+      end
+
+      expect(current_path).to eq('/logout')
     end
 
+    it "I can see a cart indicator on all pages" do
+      visit '/merchants'
+
+      within 'nav' do
+        expect(page).to have_content("Cart: 0")
+      end
+
+      visit '/items'
+
+      within 'nav' do
+        expect(page).to have_content("Cart: 0")
+      end
+    end
+
+    it "I can see text emplaining that I'm logged in" do
+
+      expect(page).to have_content("Logged in as User Name")
+    end
   end
 end
