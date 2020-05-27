bc040960b9dc11ca3f305690b011a3bc0880d1a9
Antonio Jackson <antoniojackson@gmail.com>
2020-05-27 17:51:34 -0600

    Adding functionality for User Story 5
    Co-Authored-By: Mad Hallé <mad.halle@icloud.com>

--- config/routes.rb
+++ config/routes.rb
@@ -35,4 +35,7 @@ Rails.application.routes.draw do
   get "/orders/new", to: "orders#new"
   post "/orders", to: "orders#create"
   get "/orders/:id", to: "orders#show"
+
+  get "/admin", to: "orders#index"
+  get "/admin/users", to: "users#index"
 end
