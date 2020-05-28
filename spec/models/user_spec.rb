da8c12fab047642713e8f775620641b5e5c15ffb
Antonio Jackson <antoniojackson@gmail.com>
2020-05-27 22:44:23 -0600

    Adding validations model test for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe User do
+describe User, type: :model do
   describe "roles" do
     it "can be created as an admin" do
       user = User.create(name: "Fiona",
@@ -44,4 +44,13 @@ describe User do
     end
   end
 
+  describe "validations" do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:address)}
+    it {should validate_presence_of(:city)}
+    it {should validate_presence_of(:state)}
+    it {should validate_presence_of(:zip)}
+    it {should validate_uniqueness_of(:email)}
+    it {should validate_presence_of(:password)}
+  end
 end
