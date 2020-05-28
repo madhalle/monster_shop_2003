0ea1c072d9dd40963c356887f0c0105b70267359
Antonio Jackson <antoniojackson@gmail.com>
2020-05-27 22:46:28 -0600

    Adding updates for User Story 10 & 11 functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,21 @@ class UsersController < ApplicationController
   def show
     render file: "/public/404" unless current_user
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      flash[:notice] = 'Success! You are now registered and logged in as a User!'
+      redirect_to '/profile'
+    else
+      flash[:notice] = 'All fields are required. Please enter information in all fields.'
+      redirect_to '/register'
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :address, :city, :state, :zip, :email, :password, :password_confirmation)
+  end
 end
