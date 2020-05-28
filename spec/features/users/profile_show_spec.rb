0b2fd6195bdd76da9a2bf8eb255fa21a81955652
Taylor Keller <takeller17@gmail.com>
2020-05-28 17:29:51 -0600

    Add spec for a user seeing their profile data. Test not passing because login functionality needed

---
+++ spec/features/users/profile_show_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+
+describe "As a default user" do
+  describe "when I visit my profile page" do
+    it "I can see all profile data except for my password" do
+      user = User.create(name: "Fiona",
+                         address: "123 Top Of The Tower",
+                         city: "Duloc City",
+                         state: "Duloc State",
+                         zip: 10001,
+                         email: "p.fiona12@castle.co",
+                         password: "boom",
+                         role: 0)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      session[:user_id] = User.last.id
+      visit "/user/profile"
+
+
+      expect(page).to have_content(user.name)
+      expect(page).to have_content(user.address)
+      expect(page).to have_content(user.city)
+      expect(page).to have_content(user.state)
+      expect(page).to have_content(user.zip)
+      expect(page).to have_content(user.email)
+    end
+
+    it "I can see a link to edit my profile data" do
+      user = User.create(name: "Fiona",
+                         address: "123 Top Of The Tower",
+                         city: "Duloc City",
+                         state: "Duloc State",
+                         zip: 10001,
+                         email: "p.fiona12@castle.co",
+                         password: "boom",
+                         role: 0)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit "/user/profile"
+
+      expect(page).to have_link("Edit Profile")
+    end
+  end
+end
