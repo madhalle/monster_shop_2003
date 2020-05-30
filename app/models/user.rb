b3728322729606419ea06fb63022d672c9f92ef8
Taylor Keller <takeller17@gmail.com>
2020-05-30 13:19:13 -0600

    Add unique_item_count and total_item_count to order model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,13 @@ class User < ApplicationRecord
                         :zip
   validates_uniqueness_of :email
   validates_presence_of :password, require: true
-
+  has_many :orders
   has_secure_password
 
   enum role: %w(user merchant admin)
 
   def self.unique_email?(email)
     return false if pluck(:email).include?(email)
-    true 
+    true
   end
 end
