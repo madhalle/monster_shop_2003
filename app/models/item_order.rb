59dab7701040d5973ec7034c3b81edb26600026c
Ana Perez Santos <a.jperez@yahoo.com>
2020-05-29 22:25:21 -0600

    Begin defining top five class method
    Co-Authored-By: Antonio Jackson <AntonioJacksonII@users.noreply.github.com>

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -7,4 +7,10 @@ class ItemOrder <ApplicationRecord
   def subtotal
     price * quantity
   end
+
+  def top_five
+    # require "pry"; binding.pry
+    # Items.joins(:orders).select("orders.item_id, count(orders.iteam_id) as count").order("count desc").group("orders.item_id")
+    # Item.joins("INNER JOIN t ON t.item_id = items.id").from(ItemOrder.select("item_order.item_id, COUNT(item_order.id) as count").group("item_order.item_id").order("count DESC").limit(5), :t)
+  end
 end
