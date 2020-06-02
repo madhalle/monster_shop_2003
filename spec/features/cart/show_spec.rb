abe3c6b6709c2f8d8fcce33975bb23312d146aeb
Antonio Jackson <antoniojackson@gmail.com>
2020-06-02 08:28:46 -0600

    Adding tests for adding quantity above inventory and decreasing quantity to 0

--- spec/features/cart/show_spec.rb
+++ spec/features/cart/show_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe 'Cart show' do
         @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
         @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
 
-        @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-        @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
-        @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+        @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 2)
+        @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 2)
+        @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 2)
         visit "/items/#{@paper.id}"
         click_on "Add To Cart"
         visit "/items/#{@tire.id}"
@@ -68,6 +68,30 @@ RSpec.describe 'Cart show' do
             click_on "Remove 1 from Quantity"
             expect(page).to have_content("2")
           end
+        end
+      end
+
+      xit 'I cannot increase the quantity of items past the inventory' do
+        visit '/cart'
+
+        @items_in_cart.each do |item|
+          within "#cart-item-#{item.id}" do
+            click_on "Add 1 to Quantity"
+            expect(page).to have_content("2")
+            click_on "Add 1 to Quantity"
+            expect(page).to have_content("No More Can Be Ordered At This Time")
+          end
+        end
+      end
+
+      it 'If I decrement the quantity to 0 the item is immediately removed from my cart' do
+        visit '/cart'
+
+        @items_in_cart.each do |item|
+          within "#cart-item-#{item.id}" do
+            click_on "Remove 1 from Quantity"
+          end
+          expect(page).to_not have_css("#cart-item-#{item.id}")
         end
       end
     end
