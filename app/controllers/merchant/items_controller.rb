29fe0d1a6ac967277dd8f906ce4f2c45fa463e97
Antonio Jackson <antoniojackson@gmail.com>
2020-06-02 15:24:02 -0600

    Updating spec to add a role to @meg_user and add an inactive status to @chain item

---
+++ app/controllers/merchant/items_controller.rb
@@ -0,0 +1,6 @@
+class Merchant::ItemsController < Merchant::BaseController
+
+  def index
+    @merchant = Merchant.find(current_user.merchant_id)
+  end
+end
--- spec/features/merchant/items_index_spec.rb
+++ spec/features/merchant/items_index_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe "Merchant Items Index Page" do
   describe "When a merchant visits their items index page" do
     before(:each) do
       @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      @meg_user = @meg.users.create(name: "Meg", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203, email: "meg@megsbikes.org", password: "password")
+      @meg_user = @meg.users.create!(name: "Meg", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203, email: "meg@megsbikes.org", password: "password", role: 1)
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-      @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5, active?: false)
 
       @brian = Merchant.create(name: "Brian's Dog Shop", address: '123 Dog Rd.', city: 'Denver', state: 'CO', zip: 80204)
       @shifter = @brian.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
@@ -22,7 +22,7 @@ RSpec.describe "Merchant Items Index Page" do
         expect(page).to have_content("Price: $#{@tire.price}")
         expect(page).to have_css("img[src*='#{@tire.image}']")
         expect(page).to have_content("Active")
-        expect(page).to_not have_content(@tire.description)
+        expect(page).to have_content(@tire.description)
         expect(page).to have_content("Inventory: #{@tire.inventory}")
       end
 
@@ -30,8 +30,8 @@ RSpec.describe "Merchant Items Index Page" do
         expect(page).to have_content(@chain.name)
         expect(page).to have_content("Price: $#{@chain.price}")
         expect(page).to have_css("img[src*='#{@chain.image}']")
-        expect(page).to have_content("Active")
-        expect(page).to_not have_content(@chain.description)
+        expect(page).to have_content("Inactive")
+        expect(page).to have_content(@chain.description)
         expect(page).to have_content("Inventory: #{@chain.inventory}")
       end
 
