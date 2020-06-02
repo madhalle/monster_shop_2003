b98df8b6a878aac85a8326cef57288c33bbba7bb
Taylor Keller <takeller17@gmail.com>
2020-06-02 11:58:49 -0600

    Add route for ship action

--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,7 @@ Rails.application.routes.draw do
     get "/", to: "orders#index"
     get "/users", to: "users#index"
     get "/users/:id", to: "users#show"
+    patch "/orders/:id/ship", to: "orders#ship"
   end
 
   # get "/profile", to: "users#show"
