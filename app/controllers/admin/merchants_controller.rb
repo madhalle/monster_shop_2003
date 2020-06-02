e0fda045943b1cb06ae3e20ed8ad2130d547817e
Ana Perez Santos <a.jperez@yahoo.com>
2020-06-02 16:39:38 -0600

    Add conditional to update action, update merchant_params

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,12 +6,17 @@ class Admin::MerchantsController < Admin::BaseController
 
   def update
     @merchant = Merchant.find(merchant_params[:merchant_id])
+    if merchant_params[:type] == "disable"
+      @merchant.items.each do |item|
+        item.update(:active? => false)
+      end
+    end
     flash[:update] = "#{@merchant.name}'s account has been disabled."
     redirect_to "/admin/merchants"
   end
 
   private
     def merchant_params
-      params.permit(:merchant_id)
+      params.permit(:type, :merchant_id)
     end
 end
