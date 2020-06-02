a020c4247c05dfc79f9b6980606ab176ab01b756
Antonio Jackson <antoniojackson@gmail.com>
2020-06-02 14:06:13 -0600

    Updating Merchant Items Index Page tests and adding the route to routes

--- spec/features/merchant/items_index_spec.rb
+++ spec/features/merchant/items_index_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Merchant Items Index Page" do
-  describe "When I visit the merchant items page" do
+  describe "When a merchant visits their items index page" do
     before(:each) do
       @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @meg_user = @meg.users.create(name: "Meg", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203, email: "meg@megsbikes.org", password: "password")
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-      @shifter = @meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+
+      @brian = Merchant.create(name: "Brian's Dog Shop", address: '123 Dog Rd.', city: 'Denver', state: 'CO', zip: 80204)
+      @shifter = @brian.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@meg_user)
     end
 
-    it 'shows me a list of that merchants items' do
-      visit "merchants/#{@meg.id}/items"
+    it 'shows a list of their items with their information' do
+      visit "merchant/items"
 
       within "#item-#{@tire.id}" do
         expect(page).to have_content(@tire.name)
@@ -30,14 +35,7 @@ RSpec.describe "Merchant Items Index Page" do
         expect(page).to have_content("Inventory: #{@chain.inventory}")
       end
 
-      within "#item-#{@shifter.id}" do
-        expect(page).to have_content(@shifter.name)
-        expect(page).to have_content("Price: $#{@shifter.price}")
-        expect(page).to have_css("img[src*='#{@shifter.image}']")
-        expect(page).to have_content("Inactive")
-        expect(page).to_not have_content(@shifter.description)
-        expect(page).to have_content("Inventory: #{@shifter.inventory}")
-      end
+      expect(page).to_not have_css("#item-#{@shifter.id}")
     end
   end
 end
