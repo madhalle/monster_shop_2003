c967c19756063e516beba78e7e83f7298943627a
Taylor Keller <takeller17@gmail.com>
2020-06-02 11:56:35 -0600

    Add ship action to admin order controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,13 @@
 class Admin::OrdersController < Admin::BaseController
 
   def index
-
     @sorted_orders = Order.sort_by_status
+  end
+
+  def ship
+    order = Order.find(params[:id])
+    order.update(status: "shipped")
+    redirect_to "/admin"
   end
 
 end
